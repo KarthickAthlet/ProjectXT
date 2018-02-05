6a100e697bbf83ac0be214b6596fce31e848b3b6
Karthikeyan Palanisamy <kpalan@sapient.com>
2018-02-05 13:53:52 +0530

    components generated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainContainerComponent } from './components/main-container/main-container.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainContainerComponent
   ],
   imports: [
     BrowserModule,
